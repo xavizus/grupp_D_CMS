b785ea17e4844a45d3f930841e7c6cf9f70d3441
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-02-27 14:36:22 +0100

    Beutify.

--- wp-content/themes/ourAwesomeTheme/header.php
+++ wp-content/themes/ourAwesomeTheme/header.php
@@ -12,6 +12,7 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -24,37 +25,42 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site container">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ourawesometheme' ); ?></a>
+	<div id="page" class="site container">
+		<a class="skip-link screen-reader-text"
+			href="#content"><?php esc_html_e( 'Skip to content', 'ourawesometheme' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
+				<?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+						rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+						rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
 			$ourawesometheme_description = get_bloginfo( 'description', 'display' );
 			if ( $ourawesometheme_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $ourawesometheme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				<?php endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">Navbar</a>
-        <?php
+			<nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<button class="navbar-toggler" type="button" data-toggle="collapse"
+						data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1"
+						aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<a class="navbar-brand" href="#">Navbar</a>
+					<?php
         wp_nav_menu( array(
             'theme_location'    => 'ourawesomemenu',
             'container'         => 'div',
@@ -65,8 +71,8 @@
             'walker'            => new WP_Bootstrap_Navwalker(),
 		) );
 			?>
-    </div>
-</nav>
-	</header><!-- #masthead -->
+				</div>
+			</nav>
+		</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+		<div id="content" class="site-content">
