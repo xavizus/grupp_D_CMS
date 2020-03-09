6f25f66f4ccb5764b7fe1921aaf9b890d2e5723e
Alexandre Previ <alexandre.previ@gmail.com>
2020-03-09 16:23:42 +0100

    styling om oss

--- wp-content/themes/ourAwesomeTheme/page.php
+++ wp-content/themes/ourAwesomeTheme/page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -18,24 +19,24 @@ get_header();
 <div id="primary" class="content-area mt-5">
 	<main id="main" class="site-main contentBG container about">
 		<div class="row">
-		<?php 
-		$size = is_active_sidebar( 'sidebar-1' )? "col-9" : "col-12";
-		?>
-			<div class="<?=$size?>">
-				<div class="row">
+			<?php
+			$size = is_active_sidebar('sidebar-1') ? "col-9" : "col-12";
+			?>
+			<div class="<?= $size ?>">
+				<div class="row <?= $size ?> m-1">
 					<?php
-		while ( have_posts() ) :
-			the_post();
+					while (have_posts()) :
+						the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+						get_template_part('template-parts/content', 'page');
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+						// If comments are open or we have at least one comment, load up the comment template.
+						if (comments_open() || get_comments_number()) :
+							comments_template();
+						endif;
 
-		endwhile; // End of the loop.
-		?>
+					endwhile; // End of the loop.
+					?>
 				</div>
 			</div>
 			<div class="col-3">
@@ -43,7 +44,7 @@ get_header();
 			</div>
 		</div>
 		<div class="row">
-			<?php get_footer();?>
+			<?php get_footer(); ?>
 		</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
