d42a1e48d9277937845bc01fd2810273f4b54373
Alexandre Previ <alexandre.previ@gmail.com>
2020-03-09 17:10:25 +0100

    styling search by property type

--- wp-content/themes/ourAwesomeTheme/archive.php
+++ wp-content/themes/ourAwesomeTheme/archive.php
@@ -9,7 +9,6 @@
 
 get_header();
 ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main contentBG container">
 
@@ -35,7 +34,7 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', 'search' );
 
 			endwhile;
 
